2ae2aaec93a50ee839dd4604868f54d679b37518
SissioMisio <40896232+SissioMisio@users.noreply.github.com>
2023-06-16 17:56:05 +0200

    Update Android.mk with new generated values

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter lemonade lemonadep lunaa martini cupidr,$(TARGET_DEVICE)),)
+ifneq ($(filter cupidr ice lemonade lemonadep lunaa martini porsche,$(TARGET_DEVICE)),)
 
 endif
